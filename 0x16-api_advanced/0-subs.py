61b5e9c00ea5f4073fea2c45478e0146439a469f
nancymoreen <nancymoreen@gmail.com>
2024-01-18 14:00:23 -0800

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
-""" a function that queries the Reddit API """
+"""
+This module provides a function to query the Reddit API and return the number of subscribers for a given subreddit.
+"""
 
-import sys
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """a function that queries the Reddit API"""
-    headers = {"User-Agent": "MyBot/0.0.1"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Get the number of subscribers for a subreddit.
 
-    # Make a GET request to the Reddit API
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom_user_agent'}  # Set a custom User-Agent to avoid Too Many Requests error
+
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except KeyError:
+            # Handle the case where the expected data structure is not found
+            return 0
+    elif response.status_code == 404:
+        # Subreddit not found
+        return 0
     else:
-        return
+        # Handle other HTTP response codes if needed
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print("{:d}".format(number_of_subscribers(subreddit)))
+
