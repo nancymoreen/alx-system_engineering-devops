bb4a38990d80d7f0e61db84e029b82f1e2ca80e1
nancymoreen <nancymoreen@gmail.com>
2024-02-03 22:13:51 -0800

    modified

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code == 200:
+        results = response.json().get("data")
+        subscribers_count = results.get("subscribers")
+        print(subscribers_count)
+        return subscribers_count
+    elif response.status_code == 404:
+        print(0)
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    else:
+        print(0)
+        return 0
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        number_of_subscribers(sys.argv[1])
+
